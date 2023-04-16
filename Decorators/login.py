68929fa99811c0006d34caca7ca09f961f9b7dae
ObedRav <obedrayo2356@gmail.com>
2023-04-16 15:39:44 -0500

    Adding some doc

--- Decorators/login.py
+++ Decorators/login.py
@@ -1,16 +1,66 @@
 def logged(function):
+    """
+    Overview
 
+    The logged function is a decorator function that can be used to log the return
+    values of any function that it decorates. The decorator function takes in another
+    function as an argument and returns a wrapped version of that function that logs
+    its return value to a log file named logfile.txt.
+
+    Parameters
+
+    function (function): The function to be decorated.
+
+    Return Value
+
+    The logged function returns a new function that is a wrapped version of the original function.
+
+    Usage
+
+    To use the logged decorator, first define a function that you want to log
+    the return values for. Then, apply the @logged decorator to that function.
+
+    For example, in the code given above, the add function is defined and
+    then decorated with @logged as follows:
+
+    @logged
+    def add(x, y):
+        return x + y
+
+    Once the add function is decorated with @logged, any call to add will be logged to the logfile.txt file.
+
+    Implementation
+
+    The logged decorator function takes in another function as an argument and returns
+    a wrapped version of that function that logs its return value to a log file named logfile.txt.
+
+    The wrapped function, named wrapper, takes in any number of positional and keyword
+    arguments using the *args and **kwargs syntax. The wrapped function then calls the
+    original function, passing in the same arguments that were passed to the wrapper function.
+    The return value of the original function is stored in the value variable.
+    """
     def wrapper(*args, **kwargs):
+
         value = function(*args, **kwargs)
         with open('logfile.txt', 'a+') as f:
             fname = function.__name__
             f.write(f"{fname} returned value {value}\n")
         return value
-    
+
     return wrapper
 
 @logged
-def add(x, y):
+def add(x: int, y: int) -> float:
+    """
+    Parameters
+
+    x (int or float): The first number to be added.
+    y (int or float): The second number to be added.
+
+    Return Value
+
+    The add function returns the sum of the two input numbers.
+    """
     return x + y
 
 add(10, 20)
--- Decorators/timing.py
+++ Decorators/timing.py
@@ -1,7 +1,20 @@
 # Timing
 import time
 
-def timed(function):
+def timed(function: function):
+    """
+    The timed decorator is a Python decorator that can be used to time the execution of a
+    function. The decorator takes a single argument, which is the function to betimed.
+
+    Parameters
+
+    function (function): The function to be timed.
+
+    Return Value
+
+    The timed decorator returns a wrapped version of the input function that times the
+    execution of the function and prints the execution time to the console.
+    """
 
     def wrapper(*args, **kwargs):
         before = time.time()
@@ -16,6 +29,9 @@ def timed(function):
 
 @timed
 def my_func(x: int):
+    """
+    Function to test the timed decorator
+    """
     result = 1
     for i in range(1, x):
         result *= i
